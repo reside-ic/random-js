750772d4426e2671a09dc9fd05935b4955a62a95
Rich FitzJohn <rich.fitzjohn@gmail.com>
2022-07-14 14:52:01 +0100

    Add general random class

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ export {RngState} from "./state";
 export {RngStateBuiltin} from "./state-builtin";
 export {RngStateObserved} from "./state-observed";
 export {RngStateReplay} from "./state-replay";
+export {Random} from "./random";
 
 export {binomial} from "./binomial";
 export {exponential, randomExponential} from "./exponential";
 export {normal, randomNormal} from "./normal";
-export {uniform} from "./uniform";
+export {randomUniform, uniform} from "./uniform";
